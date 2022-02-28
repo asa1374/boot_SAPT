794206b3ce7a10b2bf1d31d7a53a74b3b932decf
changjun <changjun@DESKTOP-OR6HJGR>
2022-02-28 10:36:01 +0900

    first commit

---
+++ sapt/src/test/java/com/pro/sapt/SaptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.sapt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
