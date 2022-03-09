6f68d3f2b962f3c941648aacc7f6fc73c6c39a9a
changjun <changjun@DESKTOP-OR6HJGR>
2022-03-09 09:34:32 +0900

    first settings

---
+++ sapt/src/main/java/com/pro/sapt/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.pro.sapt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "main/main";
+	}
+
+}
